fb99b017f6c97b73b8e4aa6b3ee501c045ee78b6
ekzarov ekzarov <ekzarov@gmail.com>
2018-04-30 15:17:37 +0300

    no message

---
+++ Assets/Scripts/AICar.cs
@@ -0,0 +1,39 @@
+﻿//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class AICar : MonoBehaviour {
+
+//    public Transform pathGroup;
+//    public WheelCollider wheelFL, wheelFR;
+
+//    private List<Transform> path;
+//    private float maxSteer = 15.0f;
+//    private int currentPathObj;
+
+//	void Start () {
+//        Debug.Log("START");
+//        GetPath();
+//	}
+
+//    void GetPath()
+//    {
+//        var pathObjs = pathGroup.GetComponentInChildren<Transform>();
+//        path = new List<Transform>();
+
+//        foreach (Transform pathObj in pathObjs)
+//        {
+//            if (pathObj != transform)
+//                path.Add(pathObj);
+//        }
+//    }
+//    void Update () {
+//        GetSteer();
+//    }
+
+//    void GetSteer(){
+//        var steerVector = transform.InverseTransformPoint(new Vector3(path[currentPathObj].position.x, transform.position.y, path[currentPathObj].position.z));
+//        var newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
+//        wheelFL.steerAngle = newSteer;
+//        wheelFR.steerAngle = newSteer;
+//    }
+//}
---
+++ Assets/Scripts/PathScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PathScript : MonoBehaviour {
+
+    public List<Transform> path;
+    public Color rayColor = Color.white;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+
+        path = new List<Transform>();
+        var pathObjs = GetComponentInChildren<Transform>();
+
+        foreach (Transform pathObj in pathObjs)
+        {
+            if (pathObj != transform)
+                path.Add(pathObj);
+        }
+
+        for (var i = 0; i < path.Count; i++)
+        {
+            var pos = path[i].position;
+            if (i > 0)
+            {
+                var prev = path[i - 1].position;
+                Gizmos.DrawLine(prev, pos);
+                Gizmos.DrawWireSphere(pos, 0.5f);
+            }
+        }
+    }
+}
